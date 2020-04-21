d9a66f8cdf1f9951faeba78027bd14a107e85b7e
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 17:38:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,6 @@ test_arr = ["element1", "elememt2"]
 def oxford_comma(array)
   length = array.length
   new_string = array.join(", ")
+  puts new_string
 end
-
-puts oxford_comma(test_arr)
+oxford_comma(test_arr)
