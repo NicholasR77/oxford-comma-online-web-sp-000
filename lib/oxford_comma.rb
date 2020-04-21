c5408527db871dff6fa594c2d695edee2e71a2b1
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 17:45:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,10 @@ test_arr3 = ["element1", "element2", "element2", "element4"]
 
 def oxford_comma(array)
   length = array.length
-  if (length > 1)
-    array.join(" and ")
-  elsif (length > 2)
+  if (length > 2)
     array.join(", ")
+  elsif (length > 1)
+    array.join(" and ")
   else
     array.join("")
   end
