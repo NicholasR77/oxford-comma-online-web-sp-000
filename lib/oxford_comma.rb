d29c207bf75c9f7e16164b57ffa4db0cd32dd61f
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 17:34:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,6 @@ test_arr = ["element1", "elememt2"]
 def oxford_comma(array)
   length = array.length
   array.each.with_index do |element, index|
-
     if (index == array.last)
       puts "Yay"
     end
