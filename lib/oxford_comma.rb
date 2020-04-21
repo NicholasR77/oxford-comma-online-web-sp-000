ea8af4bf0e4cfb148d280a4c01a5c6a849e82929
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 17:37:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,12 +2,7 @@ test_arr = ["element1", "elememt2"]
 
 def oxford_comma(array)
   length = array.length
-  array.each.with_index do |element, index|
-    if (element == array.last)
-      puts "Yay"
-    else
-      puts "#{element} and #{index}"
-    end
+  array.join(" ,")
   end
 end
 
